58ff281ba33de361dee060c35c293277020d6961
ShawnCholeva <shawncholeva@gmail.com>
2017-12-10 18:50:34 -0600

    Added SafeMath library and methods to Token contract to avoid overflow/underflow attacks

--- contracts/NitrousToken.sol
+++ contracts/NitrousToken.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.18;
 
-interface IERC20Token {
+import './SafeMath.sol';
+
+ interface IERC20Token {
     function totalSupply() public constant returns (uint256 totalSup);
     function balanceOf(address _owner) public constant returns (uint256 balance);
     function transfer(address _to, uint256 _value) public returns (bool success);
@@ -13,18 +15,18 @@ interface IERC20Token {
 
 contract NitrousToken is IERC20Token {
 
+    using SafeMath for uint256;
+
     uint public constant supply = 100000000;
     string public constant symbol = "NOS";
     string public constant name = "Nitrous Token";
     uint8 public constant decimals = 18;
-    string public constant version = "1";
 
     mapping(address => uint256) balances;
     mapping(address => mapping(address => uint256)) allowed;
 
     function NitrousToken() public {
         balances[msg.sender] = supply;
-        transfer(0x9A7d0E14446EF6abB67cA71AcD5BEb03a5B2dBe7, 55000);
     }
 
     function totalSupply() public constant returns (uint256) {
@@ -41,8 +43,8 @@ contract NitrousToken is IERC20Token {
             _value > 0
         );
 
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
         Transfer(msg.sender, _to, _value);
         return true;
     }
@@ -53,9 +55,9 @@ contract NitrousToken is IERC20Token {
             balances[_from] >= _value && balances[_from] > 0
         );
 
-        balances[_from] -= _value;
-        balances[_to] += _value;
-        allowed[_from][msg.sender] -= _value;
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
         Transfer(_from, _to, _value);
         return true;
     }
